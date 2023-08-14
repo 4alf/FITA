22f1030438169753afc9815083ab10e5968f864b
4alf <kingalifa11@gmail.com>
2023-08-14 06:51:03 +0100

    create pagination

---
+++ src/components/PaginationMark.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="rellax pagination-mark" data-rellax-speed="0"></div>
+  </template>
+
+<script>
+export default {
+  name: 'PaginationMark',
+};
+</script>
+
+  <style lang="stylus" scoped>
+  .pagination-mark {
+    border: solid thin var(--border-color--primary);
+    margin-top: 0.5em;
+    height: 1.5em;
+    align-self: center;
+  }
+  </style>
